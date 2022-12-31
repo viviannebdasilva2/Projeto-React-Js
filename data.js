cf6eb8c0e540c08beb0d4665efb92700747e3360
viviannebdasilva2 <120605087+viviannebdasilva2@users.noreply.github.com>
2022-12-30 23:58:16 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,31 @@
+const data = {
+    products : [
+            { 
+            id: '1',
+            name: 'Caneca Sonhos',
+            price: 30.00,
+            img:'https://picsum.photos/id/30/1280/901',
+        },
+            {
+            id: '2',
+            name: 'Relógio Life',
+            price: 450.00,
+            img:'https://picsum.photos/id/175/2896/1944',
+        },
+            {
+            id: '3',
+            name: 'Sapatos Renderi',
+            price: 40.00,
+            img:'https://picsum.photos/id/21/3008/2008',
+        },
+            {
+            id: '4',
+            name: 'Toca Discos',
+            price: 40.00,
+            img:'https://picsum.photos/id/39/3456/2304',
+        },
+    ]
+    }
+    
+    export default data;
+    
